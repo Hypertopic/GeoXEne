34918d9656199871ae3fe0cc0a613139e6bd1fe2
Aurelien Benel <aurelien.benel@utt.fr>
2022-05-08 19:02:39 +0200

    PROCESS: API key should not be required to setup a development server.

--- public/maps.js
+++ public/maps.js
@@ -1,3 +1,22 @@
+const settings = {
+  coverage: {
+    draggableCursor: 'pointer',
+    center: {lat: 37.977950, lng: 23.718200},
+    zoom: 18,
+    mapTypeId: 'satellite'
+  },
+  overlays: [
+    [
+      'https://steatite.utt.fr/picture/405e66c63060b595c02e6590a8f6abbda104cb03',
+      {south: 37.977910, west: 23.718200, north: 37.978780, east: 23.719010}
+    ],
+    [
+      'https://steatite.utt.fr/picture/78e81db6ee77decd3e40cf3124c04a6aaaaa2fc4',
+      {south: 37.97862453543905, west: 23.716649743986387, north: 37.978755, east: 23.716789}
+    ]
+  ]
+};
+
 let createMap = (id) => new google.maps.Map(document.getElementById(id), settings.coverage);
 
 let fillForm = (latitude, longitude, title = '') => {
@@ -16,7 +35,7 @@ let putOverlaysOnMap = (map) => settings.overlays.map(x => new google.maps.Groun
     x.addListener('click', showLocation);
   });
 
-let getLocation = (address) => fetch(`/json?address=${address}&key=${settings.key}`)
+let getLocation = (address) => fetch(`/json?address=${address}`)
   .then(x => x.json())
   .then(x => ({
     title: address,
